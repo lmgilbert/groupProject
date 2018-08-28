6a0f18296ce85916a410f853b0cca9265b9fb58f
Lauren <laurengilbert1997@gmail.com>
2018-08-28 13:54:19 +1200

    Added titles

--- src/components/lauren.vue
+++ src/components/lauren.vue
@@ -1,35 +1,62 @@
+
 <template>
+
     <div class="body">
         <h1>
             This is lauren's component 
             
         </h1>
-        <div v-for="info in infos" :key="info">
-            {{ info.name }}
+        <div>
+            <h2 v-for="fact in info" :key="fact">{{ fact.name }}</h2>
+            <span>Height: </span><span v-for="fact in info" :key="fact">{{ fact.height }}</span><br>
+            <span>Hair Colour: </span><span v-for="fact in info" :key="fact">{{ fact.hair_color }}</span><br>
+            <span>Skin Colour: </span><span v-for="fact in info" :key="fact">{{ fact.skin_color }}</span><br>
+            <span>Eye Colour: </span><span v-for="fact in info" :key="fact">{{ fact.eye_color }}</span><br>
+            <span>Birth Year: </span><span v-for="fact in info" :key="fact">{{ fact.birth_year }}</span><br>
+            <span>Gender: </span><span v-for="fact in info" :key="fact">{{ fact.gender }}</span><br>
+            <!--<p>{{ fact.height }}</p>
+            <p> {{ fact.hair_color }}</p>
+            <p>{{ fact.skin_color }}</p>
+            <p>{{ fact.eye_color }}</p>
+            <p>{{ fact.birth_year }}</p>
+            <p>{{ fact.gender }}</p>-->
         </div>
     </div>
 </template>
 
 <script>
+
 import axios from 'axios'
 export default {
     name: "lauren",
     data () {
       return {
-          hello: "blah",
-          infos: null
+          info: null,
       }
     },
+    //https://api.giphy.com/v1/gifs/search?api_key=j5A12BRdSwlPVq8sS3qXGmuoawOgeed6&q=&limit=25&offset=0&rating=G&lang=en
     mounted () {
+        let randomNumber = Math.floor((Math.random() * 88) + 1);
+        let apiURL = 'https://swapi.co/api/people/' + randomNumber
+
     axios
-      .get('https://swapi.co/api/people')
+      .get(apiURL)
       .then(response => (this.info = response))
+      
+      
     }
+
+    
 }
 
 </script>
 
+
 <style>
+.inline {
+    display: inline;
+}
+
 h1, h2 {
   font-weight: normal;
 }
