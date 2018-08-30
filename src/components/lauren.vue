1758d114dc161f2a82016bed4369874d9a8b0e0c
Lauren <laurengilbert1997@gmail.com>
2018-08-30 12:15:09 +1200

    Played around with trying to get name of character

--- src/components/lauren.vue
+++ src/components/lauren.vue
@@ -5,15 +5,19 @@
         <h1>
             This is lauren's component 
             
+            
         </h1>
+        <!--<p>{{ name }}</p>-->
         <div>
             <h2 v-for="fact in info" :key="fact">{{ fact.name }}</h2>
             <span>Height: </span><span v-for="fact in info" :key="fact">{{ fact.height }}</span><br>
-            <span>Hair Colour: </span><span v-for="fact in info" :key="fact">{{ fact.hair_color }}</span><br>
+            <span>Hair Colour: </span><span v-for="fact in info" :key="fact">{{ fact.hair_color}}</span><br>
             <span>Skin Colour: </span><span v-for="fact in info" :key="fact">{{ fact.skin_color }}</span><br>
             <span>Eye Colour: </span><span v-for="fact in info" :key="fact">{{ fact.eye_color }}</span><br>
             <span>Birth Year: </span><span v-for="fact in info" :key="fact">{{ fact.birth_year }}</span><br>
             <span>Gender: </span><span v-for="fact in info" :key="fact">{{ fact.gender }}</span><br>
+            
+            
             <!--<p>{{ fact.height }}</p>
             <p> {{ fact.hair_color }}</p>
             <p>{{ fact.skin_color }}</p>
@@ -31,19 +35,35 @@ export default {
     name: "lauren",
     data () {
       return {
-          info: null,
+          info: [],
+          search: ''
+          //searchTerm: info.name,
+          
       }
     },
     //https://api.giphy.com/v1/gifs/search?api_key=j5A12BRdSwlPVq8sS3qXGmuoawOgeed6&q=&limit=25&offset=0&rating=G&lang=en
     mounted () {
         let randomNumber = Math.floor((Math.random() * 88) + 1);
         let apiURL = 'https://swapi.co/api/people/' + randomNumber
+    
+        axios
+        .get(apiURL)
+        .then(response => (this.info = response))
+    
+        this.search = this.info.name;
+        console.log(this.info)
+    },
+    methods: {
+        getGifs() {
+            console.log(this.info);
+        },
 
-    axios
-      .get(apiURL)
-      .then(response => (this.info = response))
-      
-      
+    },
+
+    filters: {
+        toUppercase: function(text) {
+            return text.toUpperCase();
+        }
     }
 
     
